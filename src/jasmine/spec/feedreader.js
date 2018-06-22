bcf68b8fafc6123b3429dd92c3a2c82f769a0bef
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-06-22 16:37:12 -0300

    start new test suite 'Initial Entries'

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -107,7 +107,9 @@ $(function() {
 		* Remember, loadFeed() is asynchronous so this test will require
 		* the use of Jasmine's beforeEach and asynchronous done() function.
 		*/
+	describe('Initial Entries', () => {
 
+	})
 	/* TODO: Write a new test suite named "New Feed Selection" */
 
 	/* TODO: Write a test that ensures when a new feed is loaded
