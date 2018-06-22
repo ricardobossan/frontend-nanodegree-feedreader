374f4d948d4f3e644c367f2f0d95b29698185172
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-06-22 04:57:25 -0300

    add test suite and convert app.js indentation to tabs

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -15,12 +15,12 @@ $(function() {
 	*/
 	describe('RSS Feeds', () => {
 		/* This is our first test - it tests to make sure that the
-     	* allFeeds variable has been defined and that it is not
-     	* empty. Experiment with this before you get started on
-     	* the rest of this project. What happens when you change
-     	* allFeeds in app.js to be an empty array and refresh the
-     	* page?
-     	*/
+		* allFeeds variable has been defined and that it is not
+		* empty. Experiment with this before you get started on
+		* the rest of this project. What happens when you change
+		* allFeeds in app.js to be an empty array and refresh the
+		* page?
+		*/
 		it('are defined', () => {
 			expect(allFeeds).toBeDefined();
 			expect(allFeeds.length).not.toBe(0);
@@ -28,53 +28,64 @@ $(function() {
 
 
 		/* TODO: Write a test that loops through each feed
-     	* in the allFeeds object and ensures it has a URL defined
-     	* and that the URL is not empty.
-     	*/
-		it('contain an URL', () => { });
+		* in the allFeeds object and ensures it has a URL defined
+		* and that the URL is not empty.
+		*/
+		describe('contain', () => {
+			allFeeds.forEach(function(aFeed){
+				describe(`in this feed`, () => {
+					it('an url', () => {
+						expect(Object.keys(aFeed)).toContain('url');
+					});
+					it('and that the url is not empty', () => {
+						expect(Object.entries(aFeed)[1]).toContain('url', 'http://');
+					});
+				});
+			});
+		});
 
 
 		/* TODO: Write a test that loops through each feed
-     	* in the allFeeds object and ensures it has a name defined
-     	* and that the name is not empty.
-     	*/
+		* in the allFeeds object and ensures it has a name defined
+		* and that the name is not empty.
+		*/
 	});
 
 
 	/* TODO: Write a new test suite named "The menu" */
 
 	/* TODO: Write a test that ensures the menu element is
-     	* hidden by default. You'll have to analyze the HTML and
-     	* the CSS to determine how we're performing the
-     	* hiding/showing of the menu element.
-     	*/
+		* hidden by default. You'll have to analyze the HTML and
+		* the CSS to determine how we're performing the
+		* hiding/showing of the menu element.
+		*/
 
 	/* TODO: Write a test that ensures the menu changes
-      	* visibility when the menu icon is clicked. This test
-      	* should have two expectations: does the menu display when
-      	* clicked and does it hide when clicked again.
-      	*/
+		* visibility when the menu icon is clicked. This test
+		* should have two expectations: does the menu display when
+		* clicked and does it hide when clicked again.
+		*/
 
 	/* TODO: Write a new test suite named "Initial Entries" */
 
 	/* TODO: Write a test that ensures when the loadFeed
-     	* function is called and completes its work, there is at least
-     	* a single .entry element within the .feed container.
-     	* Remember, loadFeed() is asynchronous so this test will require
-     	* the use of Jasmine's beforeEach and asynchronous done() function.
-     	*/
+		* function is called and completes its work, there is at least
+		* a single .entry element within the .feed container.
+		* Remember, loadFeed() is asynchronous so this test will require
+		* the use of Jasmine's beforeEach and asynchronous done() function.
+		*/
 
 	/* TODO: Write a new test suite named "New Feed Selection" */
 
 	/* TODO: Write a test that ensures when a new feed is loaded
-     	* by the loadFeed function that the content actually changes.
-     	* Remember, loadFeed() is asynchronous.
+		* by the loadFeed function that the content actually changes.
+		* Remember, loadFeed() is asynchronous.
 		* I MADE EVERYTHING ON THE PAGE DARKER, SO THE BRIGHTNESS IN THE BROWSER WOULDN'T ANOY ME
 		IN THE END, IN THE `DIST` FOLDER, EITHER:
 		**, LOAD THE LAST SHA BEFORE MY FIRST COMMIT ON THE REPO
 		OR
 		** FOLLOW INSTRUCTIONS IN COMMENTS IN EACH OF THE FOLLOWING FILES, ALWAYS DELETING THE COMMENTS AFTERWARDS:
-     	--- style.css
-     	--- jasmine.css
-     	*/
+		--- style.css
+		--- jasmine.css
+		*/
 }());
