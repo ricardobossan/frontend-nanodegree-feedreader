74f709752663d92f94d78946d20e23d55ef82e61
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-06-22 05:28:39 -0300

    add start of a new test suite

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -61,12 +61,13 @@ $(function() {
 				});
 			});
 		});
-
 	});
 
 
 	/* TODO: Write a new test suite named "The menu" */
+	describe('The Menu', () => {
 
+	});
 	/* TODO: Write a test that ensures the menu element is
 		* hidden by default. You'll have to analyze the HTML and
 		* the CSS to determine how we're performing the
